caa2951cfc8cd972899f3cde25762c3130ca7e4b
COSHW <roman-coshw@yandex.ru>
2019-11-15 00:57:47 +0300

    Adjusting stuff

--- hand_detector.py
+++ hand_detector.py
@@ -2,8 +2,8 @@ from __future__ import division
 import cv2
 import numpy as np
 
-protoFile = "hand/pose_deploy.prototxt"
-weightsFile = "hand/pose_iter_102000.caffemodel"
+protoFile = "model/pose_deploy.prototxt"
+weightsFile = "model/pose_iter_102000.caffemodel"
 nPoints = 22
 net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
 
